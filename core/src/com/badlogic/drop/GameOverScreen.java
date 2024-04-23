37bc1e5075b0a60a327d065661fed1782aa73611
Wagner J. Castro <wajocastro@hotmail.com>
2024-04-23 00:07:47 -0300

    Adicionando a opção de sair do jogo no modo survival

--- core/src/com/badlogic/drop/GameOverScreen.java
+++ core/src/com/badlogic/drop/GameOverScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.graphics.Texture;
 
@@ -13,7 +14,9 @@ public class GameOverScreen implements Screen {
     OrthographicCamera camera;
     int score;
     Texture menuImage;
+    Texture sairImage;
     Rectangle menu;
+    Rectangle sair;
 
 
 
@@ -22,15 +25,22 @@ public class GameOverScreen implements Screen {
         this.score = score;
 
         menuImage = new Texture(Gdx.files.internal("bMenu.png"));
+        sairImage = new Texture(Gdx.files.internal("bSair.png"));
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
 
         menu = new Rectangle();
-        menu.x = 300;
+        menu.x = 200;
         menu.y = 240;
         menu.width = 170;
         menu.height = 66;
+
+        sair = new Rectangle();
+        sair.x = 400;
+        sair.y = 240;
+        sair.width = 170;
+        sair.height = 66;
     }
 
     @Override
@@ -49,7 +59,22 @@ public class GameOverScreen implements Screen {
         game.font.draw(game.batch, "Que pena, você perdeu!", 100, 150);
         game.font.draw(game.batch, "Seu placar foi de: " + score , 100, 100);
         game.batch.draw(menuImage, menu.x, menu.y, menu.width, menu.height);
+        game.batch.draw(sairImage, sair.x, sair.y, sair.width, sair.height);
         game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+
+            if (menu.contains(touchPos.x, touchPos.y)) {
+                game.setScreen(new MainMenuScreen(game));
+            } else if (sair.contains(touchPos.x, touchPos.y)) { // se o jogador tocou no botão de saída
+                Gdx.app.exit(); // saia do jogo
+            }
+
+            dispose();
+        }
 
     }
 
